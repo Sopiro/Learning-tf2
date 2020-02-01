f5e109b26e7c5cfe342f0ce96a8067cb8a9210da
Sopiro <knwer782@naver.com>
2020-02-02 07:02:50 +0800

    Applied BatchNormalizaition, Dropout

--- src/08_mnist.py
+++ src/08_mnist.py
@@ -15,18 +15,28 @@ train_Y = tf.keras.utils.to_categorical(train_Y)
 test_Y = tf.keras.utils.to_categorical(test_Y)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(units=128, activation='relu', kernel_initializer='he_normal', input_shape=(784,)),
-    tf.keras.layers.Dense(units=64, activation='relu', kernel_initializer='he_normal'),
-    tf.keras.layers.Dense(units=32, activation='relu', kernel_initializer='he_normal'),
-    tf.keras.layers.Dense(units=10, activation='softmax', kernel_initializer='he_normal')
+    tf.keras.layers.Dense(units=128, kernel_initializer='he_normal', input_shape=(784,)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Activation('relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(units=64, kernel_initializer='he_normal'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Activation('relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(units=32, kernel_initializer='he_normal'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Activation('relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(units=10, activation='softmax', kernel_initializer='he_normal'),
 ])
 
 model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
-history = model.fit(train_X, train_Y, batch_size=32, epochs=10, verbose=1, validation_split=0.2)
+history = model.fit(train_X, train_Y, batch_size=1000, epochs=100, verbose=1, validation_split=0.2)
 
 plt.plot(history.history['accuracy'], 'b-', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'm-', label='val_accuracy')
 plt.plot(history.history['loss'], 'k--', label='loss')
 plt.legend()
 plt.ylim(0, 1)
