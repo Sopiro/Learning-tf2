f05935b76a0b5501b58a73c2868a267981622b0a
Sopiro <knwer782@naver.com>
2020-02-12 04:30:28 +0900

    Sentence generation

---
+++ src/15_sentence_generation.py
@@ -0,0 +1,118 @@
+import tensorflow as tf
+import re
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+path_to_file = tf.keras.utils.get_file('input.txt', 'http://bit.ly/2Mc3SOV')
+
+train_text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+
+
+# From https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+def clean_str(string):
+    string = re.sub(r"[^가-힣A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", "", string)
+    string = re.sub(r"\)", "", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    string = re.sub(r"\'{2,}", "\'", string)
+    string = re.sub(r"\'", "", string)
+
+    return string
+
+
+train_text = train_text.split('\n')
+train_text = [clean_str(sentence) for sentence in train_text]
+train_text_X = []
+for sentence in train_text:
+    train_text_X.extend(sentence.split(' '))
+    train_text_X.append('\n')
+
+train_text_X = [word for word in train_text_X if word != '']
+
+# print(train_text_X[:20])
+
+vocab = sorted(set(train_text_X))
+vocab.append('UNK')
+
+word2idx = {u: i for i, u in enumerate(vocab)}
+idx2word = np.array(vocab)
+
+text_as_int = np.array([word2idx[c] for c in train_text_X])
+
+seq_length = 25
+examples_per_epoch = len(text_as_int) // seq_length
+sentence_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sentence_dataset = sentence_dataset.batch(seq_length + 1, drop_remainder=True)
+
+
+def split_input_target(chunk):
+    return [chunk[:-1], chunk[-1]]
+
+
+train_dataset = sentence_dataset.map(split_input_target)
+
+BATCH_SIZE = 512
+steps_per_epoch = examples_per_epoch // BATCH_SIZE
+BUFFER_SIZE = 10000
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+# https://www.tensorflow.org/api_docs/python/tf/data/Dataset#shuffle
+train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+total_words = len(vocab)
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(total_words, 100, input_length=seq_length),
+    tf.keras.layers.LSTM(units=100, return_sequences=True),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.LSTM(units=100),
+    tf.keras.layers.Dense(total_words, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+
+# def testmodel(epoch, logs):
+#     if epoch % 5 != 0 and epoch != 49:
+#         return
+#     test_sentence = train_text[0]
+#
+#     next_words = 100
+#     for _ in range(next_words):
+#         test_text_X = test_sentence.split(' ')[-seq_length:]
+#         test_text_X = np.array([word2idx[c] if c in word2idx else word2idx['UNK'] for c in test_text_X])
+#         test_text_X = pad_sequences([test_text_X], maxlen=seq_length,
+#                                                                     padding='pre', value=word2idx['UNK'])
+#
+#         output_idx = model.predict_classes(test_text_X)
+#         test_sentence += ' ' + idx2word[output_idx[0]]
+#
+#     print()
+#     print(test_sentence)
+#     print()
+#
+#
+# testmodelcb = tf.keras.callbacks.LambdaCallback(on_epoch_end=testmodel)
+
+history = model.fit(train_dataset.repeat(), epochs=50, steps_per_epoch=steps_per_epoch, callbacks=[testmodelcb],
+                    verbose=2)
+
+
+test_sentence = '동헌에 나가 공무를 본 후 활 십오 순을 쏘았다'
+
+next_words = 100
+for _ in range(next_words):
+    test_text_X = test_sentence.split(' ')[-seq_length:]
+    test_text_X = np.array([word2idx[c] if c in word2idx else word2idx['UNK'] for c in test_text_X])
+    test_text_X = pad_sequences([test_text_X], maxlen=seq_length, padding='pre', value=word2idx['UNK'])
+
+    output_idx = model.predict_classes(test_text_X)
+    test_sentence += ' ' + idx2word[output_idx[0]]
+
+print(test_sentence)
