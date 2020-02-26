43eea489fa3b7796f54cb27a1eddbb9598be96f8
Sopiro <knwer782@naver.com>
2020-02-27 04:50:56 +0900

    Optimization

--- src/24_gan_on_mnist.py
+++ src/24_gan_on_mnist.py
@@ -7,12 +7,13 @@ import time
 (train_images, _), _ = tf.keras.datasets.mnist.load_data()
 
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-train_images = train_images / 255.0
+train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
+assert BUFFER_SIZE <= train_images.__sizeof__()
 
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images[:BUFFER_SIZE]).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
 generator = tf.keras.Sequential([
     tf.keras.layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)),
@@ -30,7 +31,7 @@ generator = tf.keras.Sequential([
     tf.keras.layers.LeakyReLU(),
 
     tf.keras.layers.Conv2DTranspose(1, kernel_size=(5, 5), strides=(2, 2), padding='same', use_bias=False),
-    tf.keras.layers.Activation('sigmoid')
+    tf.keras.layers.Activation('tanh')
 ])
 
 # generator.summary()
@@ -55,8 +56,8 @@ discriminator = tf.keras.Sequential([
     tf.keras.layers.Dense(1)
 ])
 
+# Binary_Cross_Entropy(y, h(x)) = -(y * log(h(x)) + (1 - y) * log(1 - h(x)))
 bce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
 
 # print(tf.ones_like(tf.random.normal((10, 10))))
 
@@ -75,7 +76,7 @@ def loss_g(fake_output):
 optimizer_g = tf.keras.optimizers.Adam(1e-4)
 optimizer_d = tf.keras.optimizers.Adam(1e-4)
 
-EPOCHS = 10
+EPOCHS = 30
 
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
@@ -122,17 +123,16 @@ def train(dataset, epochs):
 
         generate_and_save_images(generator, epoch + 1, seed)
 
+        # Save checkpoint every 10 epochs
         if (epoch + 1) % 10 == 0:
             checkpoint.save(file_prefix=checkpoint_prefix)
 
         print('Time for epoch {} is {} sec'.format(checkpoint.epochs.numpy(), time.time() - start))
 
-    generate_and_save_images(generator, epochs, seed)
-
 
 def generate_and_save_images(model, epoch, test_input, show=False):
-    # `training`이 False로 맞춰진 것을 주목하세요.
-    # 이렇게 하면 (배치정규화를 포함하여) 모든 층들이 추론 모드로 실행됩니다.
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
     predictions = model(test_input, training=False)
 
     fig = plt.figure(figsize=(8, 8))
@@ -140,13 +140,16 @@ def generate_and_save_images(model, epoch, test_input, show=False):
         plt.subplot(4, 4, i + 1)
         plt.imshow(predictions[i, :, :, 0] * 0.5 + 0.5, cmap='gray')
         plt.axis('off')
-    plt.savefig('../gan_result/image_at_epoch_{:04d}.png'.format(epoch))
+    plt.savefig('../gan_result/image_at_epoch_{:04d}.png'.format(checkpoint.epochs.numpy()))
     if show:
         plt.show()
     plt.close(fig)
 
 
 train(train_dataset, EPOCHS)
+
+print(discriminator(tf.expand_dims(train_images[0], 0)))
+print(discriminator(generator(tf.random.normal([1, noise_dim]))))
 
 for i in range(1):
     generate_and_save_images(generator, 1000 + i, tf.random.normal([num_examples_to_generate, noise_dim]), True)
